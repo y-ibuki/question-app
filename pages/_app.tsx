943957fcd02408760417fe3fd662706d5bf9e4ac
y-ibuki <y.ibuki91@gmail.com>
2021-03-22 00:05:50 +0900

    user概念を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { RecoilRoot } from 'recoil'
+import '../styles/globals.scss'
+import '../lib/firebase'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+  )
 }
 
 export default MyApp
